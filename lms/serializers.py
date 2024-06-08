c2203678946fafee71d93343ed57f36f660244b0
elenaludina0573 <elenaludina0@gmail.com>
2024-06-09 00:44:54 +0300

    Addendum

--- lms/serializers.py
+++ lms/serializers.py
@@ -4,9 +4,21 @@ from lms.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Course
         fields = '__all__'
+
+
+class CourseDitailSerializer(serializers.ModelSerializer):
+    number_of_lessons = serializers.SerializerMethodField()
+
+    def get_number_of_lessons(self, obj):
+        return obj.lesson_set.count()
+
+    class Meta:
+        model = Course
+        fields = ('id', 'title', 'description', 'number_of_lessons')
 
 
 class LessonSerializer(serializers.ModelSerializer):
