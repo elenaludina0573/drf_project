8e19cec4100dad9ccbc629f2a13481b3142a7dd4
elenaludina0573 <elenaludina0@gmail.com>
2024-06-09 18:20:45 +0300

    Creation

---
+++ users/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from users.models import Payment
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+
+    class Mena:
+        model = Payment
+        fields = "__all__"
