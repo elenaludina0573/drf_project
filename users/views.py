a51d56d9d508592f5e6409205443573fa6dfba7e
elenaludina0573 <elenaludina0@gmail.com>
2024-06-09 18:20:56 +0300

    Creation

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics
 
-# Create your views here.
+from users.models import Payment, User
+from users.serializers import PaymentSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+
+
+class PaymentCreateAPIView(generics.CreateAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+
+
+class PaymentListAPIView(generics.ListAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
