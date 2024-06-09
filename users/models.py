6f1da8b942e6ed7482ab494d96ab73d57b79a9ed
elenaludina0573 <elenaludina0@gmail.com>
2024-06-09 18:15:26 +0300

    The team for db

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from lms.models import Course, Lesson
+
 NULLABLE = {'blank': True, 'null': True}
 
 
@@ -21,3 +23,21 @@ class User(AbstractUser):
         verbose_name = 'пользователь'
         verbose_name_plural = 'пользователи'
 
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='Пользователь'),
+    date_of_payment = models.DateTimeField(auto_now_add=True, verbose_name='Дата оплаты'),
+    paid_course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Оплаченный курс", **NULLABLE),
+    paid_lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name="Оплаченный урок", **NULLABLE),
+    payment_sum = models.PositiveIntegerField(verbose_name='Cумма платежа')
+    method_choices = {"наличными": "наличными", "переводом": "переводом"}
+    payment_method = models.CharField(max_length=50, choices=method_choices, verbose_name='Способ оплаты')
+
+    def __str__(self):
+        return (f'{self.user}: {self.date_of_payment}, {self.payment_sum}, {self.payment_method}, '
+                f'за {self.paid_course if self.paid_course else self.paid_lesson}')
+
+    class Meta:
+        verbose_name = 'платеж'
+        verbose_name_plural = 'платежи'
+        #ordering = ['-date_of_payment']
