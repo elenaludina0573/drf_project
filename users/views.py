2275d36484fa2b3c7d2064eb18209244c3f833da
elenaludina0573 <elenaludina0@gmail.com>
2024-06-10 20:47:38 +0300

    Correction

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, generics
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from users.models import Payment, User
 from users.serializers import PaymentSerializer
@@ -16,3 +17,7 @@ class PaymentCreateAPIView(generics.CreateAPIView):
 class PaymentListAPIView(generics.ListAPIView):
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
+
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['paid_lesson', 'paid_course', 'payment_method']
+    ordering_fields = ['-date_of_payment']
