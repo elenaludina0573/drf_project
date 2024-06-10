c9d7799bf24fe457b0fa30da42ed8b351acc2a92
elenaludina0573 <elenaludina0@gmail.com>
2024-06-10 22:19:13 +0300

    Adding models

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from users.models import User
+from users.models import User, Payment
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['pk', 'email', 'is_active', 'is_staff', 'is_superuser']
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'user', 'date_of_payment', 'paid_lesson', 'paid_course', 'payment_sum', 'payment_method']
