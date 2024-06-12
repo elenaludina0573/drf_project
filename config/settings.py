f84aec0124659068325cc31e7b834c8130c0f816
elenaludina0573 <elenaludina0@gmail.com>
2024-06-12 15:37:42 +0300

    Addendum

--- config/settings.py
+++ config/settings.py
@@ -95,9 +95,12 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
-        'django_filters.rest_framework.DjangoFilterBackend'),
+        'django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+
     )
 }
 
