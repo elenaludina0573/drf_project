69ab6642eafa5a19a15f6268290491c8e11a3d47
elenaludina0573 <elenaludina0@gmail.com>
2024-06-13 21:38:44 +0300

    Adding access rights

--- config/settings.py
+++ config/settings.py
@@ -98,10 +98,10 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',),
-    'DEFAULT_PERMISSION_CLASSES': (
+    'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
 
-    )
+    ]
 }
 
 # Password validation
