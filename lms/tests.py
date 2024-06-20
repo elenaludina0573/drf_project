ec2e12ee19d37d5c96acd23ab908ca2c4c255c54
elenaludina0573 <elenaludina0@gmail.com>
2024-06-21 00:08:16 +0300

    Addendum

--- lms/tests.py
+++ lms/tests.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from django.urls import reverse
 
 
-from lms.models import Course, Lesson
+from lms.models import Course, Lesson, Subscription
 from users.models import User
 
 
@@ -50,4 +50,59 @@ class LessonTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['title'], "Основы backend-разработки")
         self.assertEqual(response.data['course'], self.course.id)
+
+    def test_lesson_delete(self):
+        self.url = reverse('lms:lessons_delete', args=(self.lesson.pk,))
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(
+            Lesson.objects.all().count(), 0)
+
+
+class SubscriptionTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(email='admin1@localhost')
+        self.course = Course.objects.create(title='Програмное обеспечение')
+        self.client.force_authenticate(user=self.user)
+        self.url = reverse('lms:subscription_create')
+
+    def test_subscription_activate(self):
+        """Тест подписки на курс"""
+        data = {
+            "user": self.user.id,
+            "course": self.course.id,
+        }
+        response = self.client.post(self.url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.json(),
+            {
+                "message": "Подписка добавлена",
+            },
+        )
+        self.assertTrue(
+            Subscription.objects.all().exists(),
+        )
+
+    def test_sub_deactivate(self):
+        """Тест отписки с курса"""
+        Subscription.objects.create(user=self.user, course=self.course)
+        data = {
+            "user": self.user.id,
+            "course": self.course.id,
+        }
+        response = self.client.post(self.url, data=data)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK,
+        )
+        self.assertEqual(
+            response.json(),
+            {
+                "message": "Подписка удалена",
+            },
+        )
+        self.assertFalse(
+            Subscription.objects.all().exists(),
+        )
 
