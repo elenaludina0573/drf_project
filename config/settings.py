a4c0717ab2c54cc3c309e201e921c12501598d4b
elenaludina0573 <elenaludina0@gmail.com>
2024-06-20 23:32:32 +0300

    Adding

--- config/settings.py
+++ config/settings.py
@@ -154,6 +154,6 @@ AUTH_USER_MODEL = 'users.User'
 # LOGOUT_REDIRECT_URL = '/'
 # LOGIN_URL = '/users/login/'
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
