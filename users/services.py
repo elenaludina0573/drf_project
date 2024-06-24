7bed64ee8d8bbae2f65bc29021ceded5c3d3f9bf
elenaludina0573 <elenaludina0@gmail.com>
2024-06-24 23:45:20 +0300

    Change_2

--- users/services.py
+++ users/services.py
@@ -2,20 +2,20 @@ import stripe
 from forex_python.converter import CurrencyRates
 from config.settings import STRIPE_API_KEY
 
+
 stripe.api_key = STRIPE_API_KEY
+stripe.api_key= "sk_test_51PUa34LHwU56b2BRoetVIgbzV5sltwv01uti8u3WcpfMCkebzD39fGwN1qGon0KjgmwoTJRQyw9cDvuOaroeVEIN00NshfQxZq"
 
 
 def create_stripe_product(instance):
-    """ Создает продукт в страйпе """
+    """Создаем stripe продукт"""
 
-    # return stripe.Product.create(
-    #     name=instance.get('name'),
-    #     description=instance.get('description'),
-    #     images=[instance.get('image')],
-    # )
-    title_product = f"{instance.paid_course}" if instance.paid_course else instance.paid_lesson
+    print(stripe.api_key)
+    print(STRIPE_API_KEY)
+
+    title_product = f'{instance.paid_course}' if instance.paid_course else f'{instance.paid_lesson}'
     stripe_product = stripe.Product.create(name=f"{title_product}")
-    return stripe_product.get("id")
+    return stripe_product.get('id')
 
 
 def convert_rub_to_dollars(payment_sum):
--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import AllowAny
 
 from users.models import Payment, User
 from users.serializers import PaymentSerializer, UserSerializer
-from users.services import convert_rub_to_dollars, create_stripe_price, create_stripe_session
+from users.services import convert_rub_to_dollars, create_stripe_price, create_stripe_session, create_stripe_product
 
 
 class UserCreateAPIView(generics.CreateAPIView):
